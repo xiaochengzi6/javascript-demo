938a3fd7003ff70576a225471fd46acd6dfc1bd8
小橙子 <a2742217795@hotmail.com>
2022-08-05 18:10:47 +0800

    js类型判断初稿

---
+++ 类型判断.js
@@ -0,0 +1,12 @@
+// js 类型判断 https://github.com/mqyqingfeng/Blog/issues/28
+function log(value){
+  console.log(typeof value)
+}
+
+log(null)
+
+// typeof number string undefined boolean object function 
+
+// javascript: BigInt Number Object String Boolean Null Undefined Symbol
+// 简单类型：Number String Null Undefined Symbol BigInt 复杂类型：Object
+
