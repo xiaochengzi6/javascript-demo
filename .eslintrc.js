a0a8c4973dde4e00ae6672c6073662cf5ce1cd9b
小橙子 <a2742217795@hotmail.com>
2022-08-16 22:53:16 +0800

    小改动

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
+// eslint学习文章： https://blog.csdn.net/jameszou707/article/details/121749796/
+
 module.exports = {
   env: {
     browser: true,
     commonjs: true,
     es2021: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  rules: {
-  }
+  rules: {}
 }
--- 原型链.js
+++ 原型链.js
@@ -1,34 +1,60 @@
+// 参考文章：https://mp.weixin.qq.com/s/1UDILezroK5wrcK-Z5bHOg
+
 // 模拟对象的属性在原型链的查找过程
-function search(obj, property){
-  let current = obj;
-  if(current){
+
+// 循环需要注意的有两点： 1、判出条件 2、循环要素
+function search(obj, property) {
+  let current = obj
+  if (current) {
     throw new Error('无对象')
   }
 
-  while(property){
-    if(({}).hasOwnPrototype.call(current, property)){
-      return current[property];
+  while (property) {
+    if ({}.hasOwnPrototype.call(current, property)) {
+      return current[property]
     }
     current = Object.getPrototypeOf(current)
   }
   return void 0
 }
-// 问题
-// Child.prototype = Object.create(Parent.prototype);和 Child.prototype = new Parent();
 
-// Child.prototype = Parent.prototype  Child.prototype = Parent.property
-var Child_1 = {
-  child: 1
+// 模仿一个 class 对象的继承性质
+
+function inheritFunc(SuperConstructor, properties) {
+  const { constructor } = properties
+
+  let SubConstructor = function (...args) {
+    // 这里就是去模仿 constructor 主要做的就是初始化内容，例如：属性的初始化
+    constructor.call(this, ...args)
+    SuperConstructor.call(this, ...args)
+  }
+  // 这里保存 prototype 也就是模仿 class 函数存储在原型上的特点
+  SubConstructor.prototype = {
+    ...properties,
+    constructor: SubConstructor
+  }
+
+  Object.setPrototypeOf(SubConstructor.prototype, SuperConstructor.prototype)
+  return SubConstructor
 }
-var Child_2 = {
-  child: 1
-}
-function Parent (){}
-var a = Child_1.prototype = Object.create(Parent.prototype)
 
-var b = Child_2.prototype = new Parent()
+const obj = inheritFunc(Object, {
+  constructor() {},
+  build() {
+    return 'build'
+  }
+})
+const user = new obj()
+console.log(user)
 
-console.log(Object.getPrototypeOf(a) === Parent.prototype)
-console.log(Object.getPrototypeOf(b) === Parent.prototype)
-console.log(Object.getPrototypeOf(a) === Object.getPrototypeOf(b))
-
+// 使用next 代替 prototype 可以用作链式调用的数据结构
+Object.defineProperty(Object.prototype, 'next', {
+  get() {
+    let next = Object.getPrototypeOf(this)
+    if (next === Object.prototype) return null
+    return next
+  },
+  set(value) {
+    return Object.setPrototypeOf(this, value)
+  }
+})
