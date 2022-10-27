5067b646a9fab868582c7c09fcd922dc9687131c
小橙子 <a2742217795@hotmail.com>
2022-10-27 13:59:48 +0800

    fix: 图片

---
+++ test/index.mjs
@@ -0,0 +1,5 @@
+import dayjs from "dayjs"
+
+var a = dayjs(new Date()).unix()
+
+console.log(a)
---
+++ 图片处理/index.js
@@ -0,0 +1,6 @@
+const exceptionIcon = require('./img/exception.png').default
+const vehicleIcon = require('./img/vehicle-icon.png').default
+
+// todo 打印后会出错
+
+console.log('exceptionIcon', exceptionIcon);
