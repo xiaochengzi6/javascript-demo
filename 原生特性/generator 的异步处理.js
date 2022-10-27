16ef0155b2ee79cadaa84947be21d207163b6c13
小橙子 <a2742217795@hotmail.com>
2022-10-28 05:46:21 +0800

    补全generator异步处理及自动化执行器函数

--- 原生特性/generator 的异步处理.js
+++ 原生特性/generator 的异步处理.js
@@ -17,7 +17,7 @@ function thunk(x) {
 // thunk 函数转换器
 
 // es5 
-function thunk_es5 (fn) {
+function thunk_es5(fn) {
   return function () {
     var args = [].slice.call(arguments)
     return function (callback) {
@@ -65,41 +65,41 @@ function thunkify(fn) {
 
 // test 
 
-function f(a, b, callback){
-  var sum = a + b 
+function f(a, b, callback) {
+  var sum = a + b
   callback(sum)
   callback(sum)
 }
 
 var ft = thunkify(f)
 var print = console.log.bind(console)
-ft(1,2)(print)
+ft(1, 2)(print)
 
 // 结合 generator 来看 使其能够自动执行
-function func(x, callback){
+function func(x, callback) {
   // 做一些异步的任务
-  setTimeout(()=>{
+  setTimeout(() => {
     callback(x)
   }, 3000)
 }
 var gen = thunkify(func)
 
-function* g(){
+function* g() {
   yield gen(1)
   yield gen(2)
   yield gen(3)
 }
 
 // 执行器使其能够自动调用
-function run (fn) {
-  var gen = fn() 
+function run(fn) {
+  var gen = fn()
 
   // next 其实可以理解成 thunk 的回调函数 接收 data 就是要异步获取到的结果 
-  function next(data){
+  function next(data) {
     // 这里可以对 data 数据做一些处理 
     console.log(data)
     var result = gen.next(data)
-    if(result.done) return 
+    if (result.done) return
     // result 执行上一次 yield 返回的对象 {value: xxx, done: false}
     // value 是 yield 表达式的值 一般是函数(thunk 返回的就是函数)
     result.value(next)
@@ -111,25 +111,118 @@ function run (fn) {
 
 run(g)
 
+// generator 函数自动执行必须要不断的取回对自身的控制权限从而能够自动执行
+// 1.回调函数。将异步操作进行包装，暴露出回调函数，在回调函数里面交回执行权。
+// 2.Promise 对象。将异步操作包装成 Promise 对象，用 then 方法交回执行权。
+
+// 将 回调函数 和 promise 结合在一起
+function run(fn) {
+  var gen = fn()
+
+  function next(data) {
+    var result = gen.next(data)
+    if (result.done) return
+    if (isPromise(result.value)) {
+      result.value.then((data) => {
+        next(data)
+      })
+    } else {
+      result.value(next)
+    }
+  }
+
+  next()
+}
+function isPromise(obj) {
+  return 'function' === typeof obj.then
+}
+
 
 // 上述是回调函数版本 尝试 promise 版本
 
-function co(fn){
-  var ctx = this 
-  return new Promise((resolve, reject) => {
-    if(typeof gen === 'function') gen = gen.call(ctx)
-    if(!gen || typeof gen.next !== 'function') return resolve(gen)
+function run(gen) {
+  var gen = gen()
+
+  return new Promise(function (resolve, reject) {
+
+    function next(data) {
+      try {
+        var result = gen.next(data)
+      } catch (e) {
+        return reject(e)
+      }
+
+      if (result.done) {
+        return resolve(result.value)
+      }
+
+      var value = toPromise(result.value)
+
+      value.then(function (data) {
+        next(data)
+      }, function (e) {
+        reject(e)
+      })
+    }
+
+    next()
+  })
+}
+
+
+// 优化 
+function run(gen) {
+
+  return new Promise(function (resolve, reject) {
+    if (typeof gen === 'function') gen = gen()
+    if (!gen || typeof gen.next !== 'function') return resolve(gen)
 
     onFulfilled()
-    function onFulfilled(res){
-      var ret 
-      try{
+
+    function onFulfilled(res) {
+      var ret
+      try {
         ret = gen.next(res)
-      }catch(err){
-        reject(err)
+      } catch (e) {
+        return reject(e)
       }
       next(ret)
     }
-    
+
+    function onRejected(err) {
+      var ret
+      try {
+        ret = gen.throw(err) // 将错误类型放入gen中看看是否 generator 函数内部是否捕获这个错误
+      } catch (e) {
+        return reject(e)
+      }
+      next(ret) // 没有产生错误说明 generator 内部捕获了错误，可以继续向下执行
+    }
+
+    function next(ret) {
+      if (ret.done) return
+      var value = toPromise(ret.value)
+      if (value && isPromise(value)) return value.then(onFulfilled, onRejected)
+      return onRejected(new TypeError('yield 返回的表达式并不是 promise 类型'))
+    }
+  })
+
+}
+
+
+
+// ---------------------- 工具函数 --------------------------
+function toPromise(obj) {
+  if (isPromise(obj)) return obj
+  if ('function' === typeof obj) return thunkToPromise(obj)
+  return obj
+}
+
+function thunkToPromise(fn) {
+  return new Promise(function (resolve, reject) {
+    fn(function (err, res) {
+      if (err) return reject(err)
+      resolve(res)
+    })
   })
 }
